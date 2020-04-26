67c926509d335ae19bf6d622b0c10aa21ee7b515
Michael FIG <mfig@agoric.com>
2020-04-26 12:17:25 -0600

    feat: add plugins for eventual send and mark as stage-1

--- packages/babel-standalone/src/preset-stage-1.js
+++ packages/babel-standalone/src/preset-stage-1.js
@@ -9,6 +9,7 @@ export default (_: any, opts: Object = {}) => {
     decoratorsLegacy = false,
     decoratorsBeforeExport,
     pipelineProposal = "minimal",
+    eventualSendTargetGlobal = "HandledPromise",
     recordAndTupleSyntax: recordAndTupleSyntax = "hash",
   } = opts;
 
@@ -23,6 +24,10 @@ export default (_: any, opts: Object = {}) => {
       [babelPlugins.syntaxRecordAndTuple, { syntaxType: recordAndTupleSyntax }],
       babelPlugins.proposalExportDefaultFrom,
       [babelPlugins.proposalPipelineOperator, { proposal: pipelineProposal }],
+      [
+        babelPlugins.proposalEventualSend,
+        { targetGlobal: eventualSendTargetGlobal },
+      ],
       babelPlugins.proposalDoExpressions,
     ],
   };
